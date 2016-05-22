9665573aac8cffe7bc4d9a9371ac168140d4ee30
kitt <bulinmartin@gmail.com>
2016-05-22 12:05:03 +0200

    Evaluation of pruning on amter done.

--- py/scripts/plot_pa_results.py
+++ py/scripts/plot_pa_results.py
@@ -143,10 +143,26 @@ def load_amter(na):
 
     return kitt_amter
 
-if __name__ == '__main__':
-    kitt_net = load_amter(na='noisy')
 
-    ''' XOR PA result '''
+def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None, label=None):
+    ax = ax if ax is not None else plt.gca()
+    if color is None:
+        color = ax._get_lines.color_cycle.next()
+    if np.isscalar(yerr) or len(yerr) == len(y):
+        ymin = y - yerr
+        ymax = y + yerr
+    elif len(yerr) == 2:
+        ymin, ymax = yerr
+    ax.plot(x, y, color=color, label=label)
+    ax.fill_between(x, ymax, ymin, color=color, alpha=alpha_fill)
+
+
+if __name__ == '__main__':
+    #na = 'noisy'
+    #kitt_net = load_amter(na=na)
+
+    ''' PA result '''
+    '''
     fig, ax1 = plt.subplots()
     pruning_steps = range(len(kitt_net['acc_mean']))
     ax1.errorbar(x=pruning_steps, y=kitt_net['acc_mean'], yerr=kitt_net['acc_std'], color='darkgreen')
@@ -166,18 +182,73 @@ if __name__ == '__main__':
     blue_patch = mpatches.Patch(color='darkblue', label='Net structure')
     plt.legend([blue_patch], [p.get_label() for p in [blue_patch]], loc='center right')
 
-    # Annotate
-    '''
-    plt.annotate('[960.20.14.]', xy=(0, 19950), horizontalalignment='center', verticalalignment='center',
-                 fontsize=11, color='darkblue')
-    '''
-
     for step in pruning_steps:
-        if step % 25 == 0 or step < 3:
-            plt.annotate(str(kitt_net['structure_mean'][step]).replace(' ', ''), xy=(step+11.5, kitt_net['n_syn_mean'][step] + 0.9 * kitt_net['n_syn_mean'][step]),
+        if step % 20 == 0 or step < 5:
+            plt.annotate(str(kitt_net['structure_mean'][step]).replace(' ', ''), xy=(step+7.5, kitt_net['n_syn_mean'][step] + 1.7 * kitt_net['n_syn_mean'][step]),
                          horizontalalignment='center', verticalalignment='center', fontsize=13, color='darkblue')
 
     plt.grid()
     plt.xlim([-1, len(pruning_steps)+1])
-    #plt.savefig('../../thesis/img/pa_result_mnist.eps', bbox_inches='tight', pad_inches=0.1)
+    #plt.savefig('../../thesis/img/pa_result_amter_'+na+'.eps', bbox_inches='tight', pad_inches=0.1)
+    plt.show()
+    '''
+
+    # Number of neurons in percentage
+    '''
+    net_names = ('nn', 'noisy', '80', 'st100')
+    name_dict = {'nn': 'nn_40_20_a', 'noisy': 'noisy_40_20_a', '80': 'nn_80_20_a', 'st100': 'nn_40_100_a', 'angle': 'nn_40_20_p', 'foot': 'nn_40_20_t'}
+    #net_names = ('nn', 'angle', 'foot')
+    inits = {'nn': (960, 20, 14), 'noisy': (960, 20, 14), '80': (1920, 20, 14), 'st100': (960, 100, 14),
+             'foot': (240, 20, 14), 'angle': (720, 20, 14)}
+    accs = {'nn': 0.9, 'noisy': 0.7, '80': 0.9, 'st100': 0.75,
+             'foot': 0.65, 'angle': 0.65}
+    nets = list()
+    for net_name in net_names:
+        nets.append(load_amter(na=net_name))
+    for n_i, (net, net_name) in enumerate(zip(nets, net_names)):
+        plt.bar(11+n_i*40, width=28, height=120, color='whitesmoke', alpha=0.2)
+        plt.boxplot([[100.0*structure[-1][l_i]/inits[net_name][l_i] for structure in net['structure']] for l_i in range(3)],
+                    positions=[15+n_i*40, 25+n_i*40, 35+n_i*40], widths=[8]*3)
+        plt.annotate(name_dict[net_name], xy=(25 + n_i * 40, 120), va='center', ha='center', backgroundcolor='moccasin',
+                     fontsize=15)
+        plt.annotate('acc: '+str(accs[net_name]), xy=(25 + n_i * 40, 112), va='center', ha='center', color='green',
+                     fontsize=13)
+        for l_i in range(3):
+            plt.annotate(inits[net_name][l_i], xy=(15+l_i*10 + n_i * 40, 105), va='center', ha='center', fontsize=13)
+    plt.xlabel('networks/layers')
+    plt.ylabel('used neurons after pruning [%]')
+    plt.ylim([0, 125])
+    plt.xlim([0, 50+40*(len(net_names)-1)])
+    plt.xticks([15, 25, 35, 55, 65, 75, 95, 105, 115, 135, 145, 155], ['I', 'H', 'O', 'I', 'H', 'O', 'I', 'H', 'O', 'I', 'H', 'O'])
+    plt.grid()
+    plt.show()
+    '''
+
+    # synapses reduction over pruning steps
+
+    #net_names = ('nn', 'noisy', '80', 'st100')
+    name_dict = {'nn': 'nn_40_20_a', 'noisy': 'noisy_40_20_a', '80': 'nn_80_20_a', 'st100': 'nn_40_100_a',
+                 'angle': 'nn_40_20_p', 'foot': 'nn_40_20_t'}
+    name_dict2 = {'nn': 'all sensors (acc: 0.9)', 'angle': 'proprioceptive only (acc: 0.65)', 'foot': 'tactile only (acc: 0.65)'}
+    net_names = ('nn', 'angle', 'foot')
+    inits = {'nn': 1948, 'noisy': 1948, '80': 38680, 'st100': 97400,
+             'foot': 5080, 'angle': 14680}
+    accs = {'nn': 0.9, 'noisy': 0.7, '80': 0.9, 'st100': 0.75,
+            'foot': 0.65, 'angle': 0.65}
+    nets = list()
+    for net_name in net_names:
+        nets.append(load_amter(na=net_name))
+    colors = ('red', 'green', 'blue', 'black', 'magenta', 'yellow', 'orange', 'brown')
+    for n_i, (net, net_name) in enumerate(zip(nets, net_names)):
+        #plt.errorbar(x=range(len(net['n_syn_mean'])), y=net['n_syn_mean'], yerr=net['n_syn_std'], color=colors[n_i], label=name_dict[net_name])
+        errorfill(x=range(len(net['n_syn_mean'])), y=net['n_syn_mean'], yerr=net['n_syn_std'], color=colors[n_i], label=name_dict2[net_name])
+    plt.xlabel('pruning step')
+    plt.ylabel('number of synapses')
+    plt.xlim([0, 200])
+    plt.ylim([0, 1000])
+    plt.grid()
+    ax = plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    #handles = [h[0] for h in handles]
+    plt.legend(handles, labels, loc='best')
     plt.show()
--- thesis/main.lol
+++ thesis/main.lol
@@ -1,5 +1,5 @@
 \defcounter {refsection}{0}\relax 
 \select@language {english}
-\contentsline {lstlisting}{\numberline {A2.1}Setting a terrain ground in main.cpp}{62}{lstlisting.199}
-\contentsline {lstlisting}{\numberline {A2.2}Rough sensory data files structure}{63}{lstlisting.207}
-\contentsline {lstlisting}{\numberline {A2.3}Sknn classifier specification \citep {misc:sknn}}{66}{lstlisting.224}
+\contentsline {lstlisting}{\numberline {A2.1}Setting a terrain ground in main.cpp}{66}{lstlisting.208}
+\contentsline {lstlisting}{\numberline {A2.2}Rough sensory data files structure}{67}{lstlisting.216}
+\contentsline {lstlisting}{\numberline {A2.3}Sknn classifier specification \citep {misc:sknn}}{70}{lstlisting.233}
